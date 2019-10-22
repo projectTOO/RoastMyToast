c4b661111080bfaff3fd61d818e915b5267fb9fa
Kemp Atkinson <kempatkinson@gmail.com>
2019-10-22 12:16:10 -0500

    changes seeds // routes are working // submit user

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,26 @@
-// Users table
-insert into users(name, email) values("Kemp", "test@gmail.com");
-insert into users(name, email) values("Fahad", "test@gmail.com");
-insert into users(name, email) values("Gio", "test@gmail.com");
-insert into users(name, email) values("John", "test@gmail.com");
-insert into users(name, email) values("Mary", "test@gmail.com");
+-- // Users table
+-- insert into users(name, email) values("Kemp", "test@gmail.com");
+-- insert into users(name, email) values("Fahad", "test@gmail.com");
+-- insert into users(name, email) values("Gio", "test@gmail.com");
+-- insert into users(name, email) values("John", "test@gmail.com");
+-- insert into users(name, email) values("Mary", "test@gmail.com");
 
-Lasagna	01-Kemp
-California Roll	02-Fahad
-Salmon Salad	01-Kemp
-Lasagna	03-Gio
-Fajitas	03-Gio
-New York Steak	02-Fahad
-Bread Puddin	05-Mary
+-- Lasagna	01-Kemp
+-- California Roll	02-Fahad
+-- Salmon Salad	01-Kemp
+-- Lasagna	03-Gio
+-- Fajitas	03-Gio
+-- New York Steak	02-Fahad
+-- Bread Puddin	05-Mary
 
-// Recipes table
-insert into recipes(name, ingredients, instructions, category, pic_url, rating) values("Lasagna", "ingr-1", "inst-1", "lunch", "/url-1", 0);
-insert into recipes(name, ingredients, instructions, category, pic_url, rating) values("California Roll", "ingr-1", "inst-1", "breakfast", "/url-1", 0);
-insert into recipes(name, ingredients, instructions, category, pic_url, rating) values("Salmon Salad", "ingr-1", "inst-1", "salad", "/url-1", 0);
-insert into recipes(name, ingredients, instructions, category, pic_url, rating) values("Lasagna", "ingr-1", "inst-1", "italian", "/url-1", 0);
-insert into recipes(name, ingredients, instructions, category, pic_url, rating) values("Fajitas", "ingr-1", "inst-1", "lunch", "/url-1", 0);
-insert into recipes(name, ingredients, instructions, category, pic_url, rating) values("New York Steak", "ingr-1", "inst-1", "dinner", "/url-1", 0);
-insert into recipes(name, ingredients, instructions, category, pic_url, rating) values("Bread Puddin", "ingr-1", "inst-1", "dessert", "/url-1", 0);
+-- -- // Recipes table
+-- insert into recipes(name, ingredients, instructions, category, image_url) values("Lasagna", "ingr-1", "inst-1", "lunch", "/url-1", 0);
+-- insert into recipes(name, ingredients, instructions, category, image_url) values("California Roll", "ingr-1", "inst-1", "breakfast", "/url-1", 0);
+-- insert into recipes(name, ingredients, instructions, category, image_url) values("Salmon Salad", "ingr-1", "inst-1", "salad", "/url-1", 0);
+-- insert into recipes(name, ingredients, instructions, category, image_url) values("Lasagna", "ingr-1", "inst-1", "italian", "/url-1", 0);
+-- insert into recipes(name, ingredients, instructions, category, image_url) values("Fajitas", "ingr-1", "inst-1", "lunch", "/url-1", 0);
+-- insert into recipes(name, ingredients, instructions, category, image_url) values("New York Steak", "ingr-1", "inst-1", "dinner", "/url-1", 0);
+-- insert into recipes(name, ingredients, instructions, category, image_url) values("Bread Puddin", "ingr-1", "inst-1", "dessert", "/url-1", 0);
 
-// Comments table
+-- -- // Comments table
 
